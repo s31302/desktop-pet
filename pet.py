2e585372649c42062f0de1efba56135e7309a764
s31302 <malgorzata.swiderek33@gmail.com>
2026-06-18 14:37:13 +0200

    edyca metody addPixmap na set_image

--- pet.py
+++ pet.py
@@ -12,33 +12,30 @@ class DesktopPet(QWidget):
         self.setGeometry(100, 100, 200, 200)
 
         self.label = QLabel(self)
-        pixmap = QPixmap("img/cat.png")
-        if pixmap.isNull():
-            print("ERROR: Nie znaleziono obrazka!")
-        else:
-            self.label.setPixmap(pixmap)
-            self.label.setScaledContents(True)
-            self.label.resize(200, 200)
+        self.set_image("cat.png")
 
     def mousePressEvent(self, event):
         # 1. Sprawdź, czy to lewy przycisk myszy
         if event.button() == Qt.MouseButton.LeftButton:
             # 2. Zmień obrazek na "głaskany"
-            self.addPixmap(QPixmap("img/happy_cat.png"))
+            self.set_image("happy_cat.png")
             self.oldPos = event.globalPosition().toPoint()
 
     def mouseReleaseEvent(self, event):
         # 3. Wróć do zwykłego obrazka po puszczeniu
-        self.addPixmap(QPixmap("img/cat.png"))
+        self.set_image("cat.png")
 
     def mouseMoveEvent(self, event):
         delta = event.globalPosition().toPoint() - self.oldPos
         self.move(self.x() + delta.x(), self.y() + delta.y())
         self.oldPos = event.globalPosition().toPoint()
 
-    def addPixmap(self, pixmap: QPixmap):
+    def set_image(self, filename):
+        path = f"img/{filename}"
+        pixmap = QPixmap(path)
+
         if pixmap.isNull():
-            print("ERROR: Nie znaleziono obrazka!")
+            print("ERROR: Nie znaleziono: {path}!")
         else:
             self.label.setPixmap(pixmap)
             self.label.setScaledContents(True)
