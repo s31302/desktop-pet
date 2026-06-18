14743ad49d7091779011d9adf454ec6dbc62df69
s31302 <malgorzata.swiderek33@gmail.com>
2026-06-18 14:33:13 +0200

    dodanie zmiany obrazka przy kliknieciu i metoda addPixmap

--- pet.py
+++ pet.py
@@ -21,13 +21,29 @@ class DesktopPet(QWidget):
             self.label.resize(200, 200)
 
     def mousePressEvent(self, event):
-        # Pozwala przeciągać okno za pomocą myszki
-        self.oldPos = event.globalPosition().toPoint()
+        # 1. Sprawdź, czy to lewy przycisk myszy
+        if event.button() == Qt.MouseButton.LeftButton:
+            # 2. Zmień obrazek na "głaskany"
+            self.addPixmap(QPixmap("img/happy_cat.png"))
+            self.oldPos = event.globalPosition().toPoint()
+
+    def mouseReleaseEvent(self, event):
+        # 3. Wróć do zwykłego obrazka po puszczeniu
+        self.addPixmap(QPixmap("img/cat.png"))
 
     def mouseMoveEvent(self, event):
         delta = event.globalPosition().toPoint() - self.oldPos
         self.move(self.x() + delta.x(), self.y() + delta.y())
         self.oldPos = event.globalPosition().toPoint()
+
+    def addPixmap(self, pixmap: QPixmap):
+        if pixmap.isNull():
+            print("ERROR: Nie znaleziono obrazka!")
+        else:
+            self.label.setPixmap(pixmap)
+            self.label.setScaledContents(True)
+            self.label.resize(200, 200)
+
 
 app = QApplication(sys.argv)
 pet = DesktopPet()
